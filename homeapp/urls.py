0944c281bcf7c827ea6f26c4b5221a66ea9da85d
adarsh <abc@gmail.com>
2022-11-18 14:18:54 +0530

    edited

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns=[
     path('loginpage',views.loginpage,name='loginpage'),
     path('register',views.register,name='register'),
     path('adminpage',views.adminpage,name='adminpage'),
+    path('user',views.user,name='user'),
+    path('worker',views.worker,name='worker'),
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def mainpage(request):
     return render(request,'index1.html')
 
 def loginpage(request):
+    if request.method=='POST':
+        username=request.POST.get('uname')
+        password=request.POST.get('pass')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            if user.is_staff:
+                return redirect('adminpage')
+        else:
+            messages.info(request,'invalid credentials')
     return render(request,'login.html')
 
 def register(request):
@@ -12,3 +23,11 @@ def register(request):
 
 def adminpage(request):
     return render(request,'inindexadm.html')
+
+def user(request):
+    return render(request,'user.html')
+
+def worker(request):
+    return render(request,'worker.html')
+
+
